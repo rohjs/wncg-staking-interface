85dee5c5215a133069be242cd4009f95c2ef5bfd
Kimbug <kimbug@planetariumhq.com>
2022-11-07 07:57:23 +0900

    🔥 Remove unused files

--- contracts/staking.ts
+++ contracts/staking.ts
@@ -4,44 +4,6 @@ import type { TransactionResponse } from '@ethersproject/providers'
 
 import { sanitizeNumber } from 'utils/num'
 
-export async function claimAllRewards(
-  contract: Contract
-): Promise<TransactionResponse> {
-  return await contract.claimAllRewards()
-}
-
-export async function claimBalRewards(
-  contract: Contract
-): Promise<TransactionResponse> {
-  return await contract.claimBALRewards()
-}
-
-export async function claimWncgRewards(
-  contract: Contract,
-  amount: string
-): Promise<TransactionResponse> {
-  return await contract.claimWNCGRewards(amount)
-}
-
-export async function earmarkRewards(
-  contract: Contract
-): Promise<TransactionResponse> {
-  return await contract.earmarkRewards()
-}
-
-export async function stakeBpt(
-  contract: Contract,
-  amount: string
-): Promise<TransactionResponse> {
-  return await contract.stake(parseEther(sanitizeNumber(amount)).toString())
-}
-
-export async function cooldown(
-  contract: Contract
-): Promise<TransactionResponse> {
-  return await contract.cooldown()
-}
-
 export async function unstakeBpt(
   contract: Contract,
   amount: string,
